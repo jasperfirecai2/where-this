1f287dfc58199dbd21d7a2f05de3877b779d2757
Jasper Borsboom <firecai2@gmail.com>
2018-12-14 09:26:10 +0100

    this part doesnt work
    oof

--- Beacon.py
+++ Beacon.py
@@ -11,19 +11,19 @@ class Beacon(object):
         self._address = address
         self._floor = input("What floor?")
         self._devices = []
-        for address, data in list(devices.items()):
-            if not self._uuid == data[0]:
-                b = Beacon(data, address)
-                self._devices.append(b)
-
-    def __init__(self, data, address):
-        self._uuid = data[0]
-        self._major = data[1]
-        self._minor = data[2]
-        self._power = data[3]
-        self._rssi = data[4]
-        self._address = address
-        self._floor = input("What floor?")
+    #     for address, data in list(devices.items()):
+    #         if not self._uuid == data[0]:
+    #             b = Beacon(data, address)
+    #             self._devices.append(b)
+    #
+    # def __init__(self, data, address):
+    #     self._uuid = data[0]
+    #     self._major = data[1]
+    #     self._minor = data[2]
+    #     self._power = data[3]
+    #     self._rssi = data[4]
+    #     self._address = address
+    #     self._floor = input("What floor?")
 
     def __str__(self):
         ret = "Beacon: address:{ADDR} uuid:{UUID} major:{MAJOR}" \
