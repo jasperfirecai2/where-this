9f4c4d70296730b22b51315bad6998267e01b1b4
Landwezen <36263286+Landwezen@users.noreply.github.com>
2018-12-14 10:28:36 +0100

    speg
    ik heb speg

--- sietse den/speech-to-tekst.py
+++ sietse den/speech-to-tekst.py
@@ -1,17 +1,14 @@
 import speech_recognition as sr
 import pyttsx3
 from gtts import gTTS
+import os
+
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
     audio = r.record(source, duration=5)
 out = r.recognize_google(audio)
 print(out)
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-for voice in voices:
-    print(voice, voice.id)
-    engine.setProperty('voice', voice.id)
-    engine.say(out)
-    engine.runAndWait()
-    engine.stop()
+tts = gTTS(text=out, lang='nl')
+tts.save("speg.mp3")
+os.system("speg.mp3")
