5b1f43b1efef57e695b76dfc0a8245cb60d34005
Jasper Borsboom <firecai2@gmail.com>
2018-12-14 20:00:19 +0100

    Devicelist
    testing stuff

---
+++ DeviceList.py
@@ -0,0 +1,21 @@
+
+class DeviceList:
+    def __init__(self, name):
+        self._Devices = []
+        self._Size = 0
+        self._Name = name
+
+    def add(self, device):
+        self._Devices.append(device)
+        self._Size = len(self._Devices)
+
+    def remove(self, index):
+        del self._Devices[index]
+
+    def __str__(self):
+        ret = "{COUNT} Devices in list {NAME}:\n".format(COUNT=self._Size, NAME=self._Name)
+        for Device in self._Devices:
+            ret += "Device: name: {NAME} address: {ADDR}" \
+                   "floor: {FLOOR} \n ID: {ID}\n" \
+                .format(NAME=Device._name, ADDR=Device._address, FLOOR=Device._floor, ID=Device._id)
+        return ret
