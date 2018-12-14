4a6be11bca379f8f5c99871628eb45f84b303e10
Landwezen <36263286+Landwezen@users.noreply.github.com>
2018-12-14 11:48:13 +0100

    device floor use voice test

--- Device.py
+++ Device.py
@@ -1,14 +1,29 @@
+import speech_recognition as sr
+from gtts import gTTS
+import os
+
 
 class Device(object):
 
     def __init__(self, nameP, addressP, devicesP):
         self._name = nameP
         self._address = addressP
-        self._floor = input("What floor?")
+        self._floor = self.mics()
         self._devices = devicesP
         for name, address in list(self._devices.items()):
             if address == self._address:
                 self._devices.remove(address)
+
+    @staticmethod
+    def mics(self):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("What floor? (use your voice!)")
+            audio = r.record(source, duration=5)
+        out = r.recognize_google(audio)
+        return out
+
+
 
     def __str__(self):
         ret = "Device: name: {NAME} address: {ADDR}" \
