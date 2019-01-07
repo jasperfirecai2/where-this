a6c1fe47183a3c7f4be1d22639de82f6e132a2e0
Jasper Borsboom <firecai2@gmail.com>
2019-01-07 09:51:38 +0100

    nvm then :(

--- init.py
+++ init.py
@@ -1,8 +1,6 @@
 import subprocess
 
 print("Fetching updates from github")
-if (subprocess.call(['./GitPull.sh']) != 0):
-    print("Updated from github, loading devices")
-    import main.py
-else:
-    print("something went wrong with updating. \n Please try again later")
+subprocess.call(['./GitPull.sh'])
+print("Updated from github, loading devices")
+import main.py
