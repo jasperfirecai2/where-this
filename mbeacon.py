c284b7cad06ab386afcd116f1d5987b6e82c698e
RNDM-IMPLOSIONS <La129Guna>
2019-01-07 10:37:26 +0100

    beacon timer changed to user input

--- mbeacon.py
+++ mbeacon.py
@@ -6,6 +6,8 @@ service = BeaconService()
 service.start_advertising("11111111-2222-3333-6666-555555555555",
             1, 1, 1, 200)
 sleeptime = input("Insert beacon time in seconds: ")
+print("Starting beacon for {sleeptime}"
+      .format(sleeptime=sleeptime))
 time.sleep(int(sleeptime))
 
 service.stop_advertising()
