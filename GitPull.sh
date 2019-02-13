762077d503e96961af9b4a38b384da4d069d2e7d
RNDM-IMPLOSIONS <La129Guna>
2019-02-13 11:29:08 +0100

    GeNeRiC eDiT

--- GitPull.sh
+++ GitPull.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-### BEGIN INIT INFO
-# Provides:        Startup
-# Required-Start:    networking
-# Default-Start:    2 3 4 5
-# Default-Stop:        S 0 1 6
-# Short-Description:    gitpull of pi
-# Description:        Starts/Stops/Restarts the where-this program
-### END INIT INFO
-#to be honest I don't know if I'll get this to work
-
 cd "/home/pi/Wedstrijd/" && git pull -f --rebase=false origin master
